37d65de1764e76787c375d67af2d83e81c69b37f
Pavel Zrazheyski <termitspy@gmail.com>
2022-11-08 18:56:52 +0300

    задача 27 - работает

---
+++ task027/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sumnum(int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result = result + number %10; // Console.WriteLine($" промежуточный результат: {result}");(проверка)
+        number /=10;  // Console.WriteLine($" промежуточное число: {number}"); (проверка)
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр числа {num} = {Sumnum(num)}");
